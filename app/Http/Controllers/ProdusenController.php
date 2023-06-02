b30ab6183735b50a6d85ba719fcae4149e95036e
Karta Kusuma <kartakusuma1199@gmail.com>
2023-06-02 21:40:15 +0700

    Creating apiIndex function

--- app/Http/Controllers/ProdusenController.php
+++ app/Http/Controllers/ProdusenController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Produsen;
 use Illuminate\Http\Request;
 
 class ProdusenController extends Controller
 {
-    //
+    public function apiIndex() {
+        $produsens = Produsen::all();
+
+        return response()->json([
+            'status' => 'success', 'data' => $produsens,
+        ]);
+    }
 }
